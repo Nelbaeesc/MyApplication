4530b1b5fa24309c9531c0d72d9e1ff786480659
Nelson Baez <nelbaeesc@alu.edu.gva.es>
2023-03-08 21:09:04 +0100

    Initial commit

---
+++ app/src/main/java/es/ieslavereda/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package es.ieslavereda.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
